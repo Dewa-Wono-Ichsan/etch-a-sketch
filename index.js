51d15bac1cb045c7da35e740051a320dfe89789a
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-04-06 14:56:57 +0700

    Create 16 x 16 grid box
    Loop DOM manipulation sixteen times to create container grid box and grid box.
    
    Every container grid box contain sixteen grid box.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const containerGridBox = document.createElement('div')
-containerGridBox.classList.toggle('container-grid-box')
-document.body.appendChild(containerGridBox)
 
-const gridBox = document.createElement('div')
-gridBox.classList.toggle('grid-box')    
-containerGridBox.appendChild(gridBox)
+for (let index = 0; index < 16; index++) {
+    const containerGridBox = document.createElement('div')
+    containerGridBox.classList.toggle('container-grid-box')
+    document.body.appendChild(containerGridBox)
+
+    for (let index = 0; index < 16; index++) {
+        const gridBox = document.createElement('div')
+        gridBox.classList.toggle('grid-box')    
+        containerGridBox.appendChild(gridBox)       
+    }
+    
+}

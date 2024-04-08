6c77319478f2a8803c85b765dc5a06c8aad47bd0
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-04-08 18:56:35 +0700

    Set opacity for grid box
    Declare variable darken contain value number zero for initial setup in function totalGrid().
    
    Set opacity with increment decimal that work every mouseover event occur in grid box.

--- index.js
+++ index.js
@@ -5,40 +5,41 @@ btn.textContent = 'request grid'
 btn.addEventListener('click', totalGrid)
 
 function totalGrid() {
-
+    
+    let darken = 0
     let total = parseInt(prompt('Type the total of grid box you want', 50))
-
+    
     if (total > 100 || total < 1) {
         return alert('choose between 1 and 100 grid box you want')
     } else {
-
-    for (let index = 0; index < total; index++) {
         
-        const gridBox2 = document.createElement('div')
-        gridBox2.classList.toggle('grid-box2')
-        document.body.appendChild(gridBox2)
-        gridBox2.style.height = `${100/total}%`
-        gridBox2.style.width = '100%'
-        gridBox2.style.display = 'flex'
-
         for (let index = 0; index < total; index++) {
-
-            const gridBox3 = document.createElement('div')
-            gridBox3.classList.toggle('grid-box3')
-            gridBox2.appendChild(gridBox3)
-            gridBox3.style.height = '100%'
-            gridBox3.style.width = `${100/total}%`
-            gridBox3.addEventListener('mouseover', () => {
-                gridBox3.style.backgroundColor = 
-                `rgb(
-                    ${255 * Math.random()}, ${255 * Math.random()}, ${255 * Math.random()}
-                    )`
-                })
             
+            const gridBox2 = document.createElement('div')
+            gridBox2.classList.toggle('grid-box2')
+            document.body.appendChild(gridBox2)
+            gridBox2.style.height = `${100/total}%`
+            gridBox2.style.width = '100%'
+            gridBox2.style.display = 'flex'
             
+            for (let index = 0; index < total; index++) {
+                
+                const gridBox3 = document.createElement('div')
+                gridBox3.classList.toggle('grid-box3')
+                gridBox2.appendChild(gridBox3)
+                gridBox3.style.height = '100%'
+                gridBox3.style.width = `${100/total}%`
+                gridBox3.addEventListener('mouseover', () => {
+                    gridBox3.style.backgroundColor = 
+                    `rgb(
+                        ${255 * Math.random()}, ${255 * Math.random()}, ${255 * Math.random()}
+                        , ${(++darken * 0.1).toFixed(1)})`
+                    })
+                    
+                    
+                }
             }
+            
         }
-
+        
     }
-
-}
